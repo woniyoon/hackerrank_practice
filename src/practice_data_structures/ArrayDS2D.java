e4b163c6f2887ee0a4c8b14d3b4ebdba461f4fef
woniyoon <woniyoon@gmail.com>
2020-04-02 20:45:48 +0900

    2D Array DS

---
+++ src/practice_data_structures/ArrayDS2D.java
@@ -0,0 +1,10 @@
+package practice_data_structures;
+
+public class ArrayDS2D {
+	public static void main(String[] args) {
+		int[][] arr = new int[3][3];
+		
+		System.out.println(arr.length);
+		System.out.println(arr[arr.length-1].length);
+	}
+}
