2c435822d652a2ebf9ed2b4596dddaa8131ae742
woniyoon <woniyoon@gmail.com>
2020-04-03 15:33:47 +0900

    Complete the code for 2D ArrayDS

--- src/practice_data_structures/ArrayDS2D.java
+++ src/practice_data_structures/ArrayDS2D.java
@@ -1,10 +1,31 @@
 package practice_data_structures;
 
 public class ArrayDS2D {
-	public static void main(String[] args) {
-		int[][] arr = new int[3][3];
-		
-		System.out.println(arr.length);
-		System.out.println(arr[arr.length-1].length);
-	}
+
+    static int hourglassSum(int[][] arr) {
+        int r = arr.length;
+        int c = arr[r-1].length;
+        int max = -81;
+        
+        
+        if(r<3 && c<3) {
+            return -1;
+        }
+
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                if ((j+2) < c && (i+2) < r) {
+                    int sum = arr[i][j]+ arr[i][j+1] + arr[i][j+2]
+                                         + arr[i+1][j+1]    
+                              + arr[i+2][j] + arr[i+2][j+1]+ arr[i+2][j+2];    
+                    
+                    if(sum > max) {
+                        max = sum;
+                    }
+                } else break;
+            }
+        }
+        
+        return max;
+    }
 }
