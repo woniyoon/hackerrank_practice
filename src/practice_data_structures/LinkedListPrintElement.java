220113b2193c9045921f710bc23e796a7388b145
woniyoon <woniyoon@gmail.com>
2020-04-05 19:47:51 +0900

    Create printLinkedList method

--- src/practice_data_structures/LinkedListPrintElement.java
+++ src/practice_data_structures/LinkedListPrintElement.java
@@ -1,16 +1,69 @@
 package practice_data_structures;
 
+import java.util.Scanner;
 
 public class LinkedListPrintElement {
 
-	static class SinglyLinkedListNode {
+	static class Node {
         public int data;
-        public SinglyLinkedListNode next;
+        public Node next;
 
-        public SinglyLinkedListNode(int nodeData) {
-            this.data = nodeData;
+        public Node(int data) {
+            this.data = data;
             this.next = null;
         }
+    }
+	
+	static class LinkedList {
+		public Node head;
+		public Node tail;
+		
+		public LinkedList() {
+			this.head = null;
+			this.tail = null;
+		}
+		
+		public void insertNode (int data) {
+			
+			Node node = new Node(data);
+			
+			if(this.head == null) {
+				this.head = node;
+			} else {
+				this.tail.next = node;
+			}
+			
+			this.tail = node;
+		}
+	}
+	
+	public static void printLinkedList(Node head) {
+		
+		while(head != null) {
+			System.out.println(head.data);
+			head = head.next;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	
+        LinkedList llist = new LinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            llist.insertNode(llistItem);
+        }
+
+        printLinkedList(llist.head);
+
+        scanner.close();
     }
 	
 }
