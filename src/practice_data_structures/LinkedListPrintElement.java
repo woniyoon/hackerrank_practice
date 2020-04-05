aba66db14e7e26a1ddc2b42e1ed256dd211d39ae
woniyoon <woniyoon@gmail.com>
2020-04-05 19:38:58 +0900

    Create Node and LinkedList class

---
+++ src/practice_data_structures/LinkedListPrintElement.java
@@ -0,0 +1,16 @@
+package practice_data_structures;
+
+
+public class LinkedListPrintElement {
+
+	static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+	
+}
